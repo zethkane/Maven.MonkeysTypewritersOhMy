432053cb34d94f512a798057c8ee9012ccd271ae
Zeth Kane <zkane@zipcodes-MBP-9.local>
2020-03-29 20:51:37 -0400

    finished part 1

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -4,5 +4,17 @@ package io.zipcoder;
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier {
+public class SafeCopier extends Copier {
+    public SafeCopier(String toCopy) {
+        super(toCopy);
+    }
+
+    @Override
+    public synchronized void run(){
+        while (stringIterator.hasNext()) {
+            if (stringIterator.hasNext()){
+                copied += stringIterator.next();
+            }
+        }
+    }
 }
